1a6213aca7e1c040e368cf0e65b8d086bb1d0171
Jens Ljungblad <jens.ljungblad@gmail.com>
2014-12-08 16:33:43 +0100

    Initial commit

---
+++ lib/godmin/uploads.rb
@@ -0,0 +1,7 @@
+require "godmin/uploads/version"
+
+module Godmin
+  module Uploads
+    # Your code goes here...
+  end
+end
---
+++ lib/godmin/uploads/version.rb
@@ -0,0 +1,5 @@
+module Godmin
+  module Uploads
+    VERSION = "0.0.1"
+  end
+end
