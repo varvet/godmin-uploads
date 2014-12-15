2e37c6fbbb0879ac8ce10bf0ce41c9d9b5496624
Jens Ljungblad <jens.ljungblad@gmail.com>
2014-12-15 12:02:44 +0100

    Updates gemspec + helper

--- godmin-uploads.gemspec
+++ godmin-uploads.gemspec
@@ -19,7 +19,7 @@ Gem::Specification.new do |gem|
   gem.test_files    = gem.files.grep(%r{^(test|spec|features)/})
   gem.require_paths = ["lib"]
 
-  gem.add_dependency "godmin", "~> 0.9.4"
+  gem.add_dependency "godmin", "~> 0.9.5"
   gem.add_dependency "refile", "~> 0.3.0"
   gem.add_dependency "mini_magick", "~> 4.0.1"
 
--- lib/godmin/uploads/helper.rb
+++ lib/godmin/uploads/helper.rb
@@ -2,12 +2,14 @@ module Godmin
   module Uploads
     module Helper
       def uploader(attachment, preview: false)
-        render partial: "godmin/uploads/uploader", locals: {
+        @template.render partial: "godmin/uploads/uploader", locals: {
           f: self, attachment: attachment, preview: preview
         }
       end
     end
   end
-end
 
-Godmin::FormBuilder.send(:include, Godmin::Uploads::Helper)
+  [FormBuilders::FormBuilder, FormBuilders::SimpleFormBuilder].each do |form_builder|
+    form_builder.send(:include, Uploads::Helper)
+  end
+end
