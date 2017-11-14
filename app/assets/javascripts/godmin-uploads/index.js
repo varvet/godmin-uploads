1c1cb42c3cc7f23b7d5b37fb8f984f6bb5fa46ee
Jens Ljungblad <jens.ljungblad@gmail.com>
2017-11-14 17:22:14 -0500

    Simplify upload component

--- app/assets/javascripts/godmin-uploads/index.js
+++ app/assets/javascripts/godmin-uploads/index.js
@@ -5,107 +5,44 @@
 //= require_tree .
 
 (function() {
-
   var initialize = function() {
-
-    $('[data-toggle="tooltip"]').tooltip()
-
     $(document).on("upload:start", ".godmin-uploads", function(e) {
-      var $el = $(this);
-      $el.removeClass("godmin-uploads-success");
-      $el.addClass("godmin-uploads-start");
-      showProgressBar($el);
+      var $el = $(e.currentTarget);
+      $el.addClass('godmin-uploads--uploading');
     });
 
     $(document).on("upload:progress", ".godmin-uploads", function(e) {
-      setProgressBarPercentage($(this), 50);
+      var $el = $(e.currentTarget);
+      $el.find(".progress-bar").css("width", 50 + "%");
     });
 
     $(document).on("upload:complete", ".godmin-uploads", function(e) {
-      var $el = $(this);
-      setProgressBarPercentage($el, 100);
-      $el.removeClass("godmin-uploads-start");
-      $el.addClass("godmin-uploads-complete");
+      var $el = $(e.currentTarget);
+      $el.find(".progress-bar").css("width", 100 + "%");
     });
 
     $(document).on("upload:success", ".godmin-uploads", function(e) {
-      var $el = $(this);
-      var fileObject = getFileObject($el);
-      setUploadPreview($el, fileObject);
-      setDownloadUrl($el, fileObject);
-      hideProgressBar($el);
-      $el.removeClass("godmin-uploads-complete");
-      $el.addClass("godmin-uploads-success");
+      var $el = $(e.currentTarget);
+      $el.find(".progress-bar").addClass("progress-bar-" + "success");
     });
 
     $(document).on("upload:failure", ".godmin-uploads", function(e) {
-      setProgressBarContext($(this), "danger");
+      var $el = $(e.currentTarget);
+      $el.find(".progress-bar").addClass("progress-bar-" + "danger");
     });
 
-    $(document).on("click", ".godmin-uploads__remove-link", function(e) {
+    $(document).on("click", ".godmin-uploads__remove-btn", function(e) {
       e.preventDefault();
-      var $el = $(this).parents(".godmin-uploads");
-      $el.removeClass("godmin-uploads-success");
-      $el.addClass("godmin-uploads-remove");
-      setDestroy($el);
-    });
 
-    $(document).on("click", ".remove__actions__undo", function(e) {
-      var $el = $(this).parents(".godmin-uploads");
-      $el.removeClass("godmin-uploads-remove");
-      $el.addClass("godmin-uploads-success");
-      unsetDestroy($el);
+      var $el = $(e.currentTarget);
+
+      $el.toggleClass("btn-danger").next().val(
+        $el.next().val() == "true" ? "" : "true"
+      );
     });
   };
-
-  var setDestroy = function($el) {
-    $el.find(".godmin-uploads__remove-field").val("true");
-  }
-
-  var unsetDestroy = function($el) {
-    $el.find(".godmin-uploads__remove-field").val("");
-  }
-  var setProgressBarPercentage = function($el, percentage) {
-    $el.find(".progress").find(".progress-bar").css("width", percentage + "%");
-    };
-
-  var setProgressBarContext = function($el, context) {
-    $el.find(".progress-bar").addClass("progress-bar-" + context);
-  };
-
-  var showProgressBar = function($el) {
-    $el.find(".progress").show();
-  };
-
-  var hideProgressBar = function($el) {
-    setTimeout(function() {
-      $el.find(".progress").slideUp();
-      setTimeout(function() {
-        setProgressBarPercentage($el, 0);
-      }, 1000);
-    }, 2000);
-  };
-
-  var setUploadPreview = function($el, fileObject) {
-    $el.find(".godmin-uploads__preview__image").attr(
-      "src", fileObject.url + "/fill/0/400/" + fileObject.id + "/" + fileObject.filename);
-  };
-
-  var setDownloadUrl = function($el, fileObject) {
-    ext = fileObject.filename.split(".").pop().toLowerCase();
-    $el.find(".godmin-uploads__preview__download__link").attr(
-      "href", fileObject.url + "/" + fileObject.id + "/" + fileObject.filename);
-  };
-
-  var getFileObject = function($el) {
-    var $file = $el.find(".godmin-uploads__file-field")
-    var fileObject = JSON.parse($file.prev().val());
-    fileObject.url = $file.data("url")
-    return fileObject;
-  };
-
+  
   $(function() {
     initialize();
   });
-
 })();
